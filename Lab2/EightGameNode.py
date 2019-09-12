17faaded4f8c519bd8928850e9d84b88dde87677
Sondre Bolland <sondrebolland@hotmail.com>
2019-09-12 19:56:12 +0200

    Added lab2

---
+++ Lab2/EightGameNode.py
@@ -0,0 +1,127 @@
+import copy
+
+# Representation of one state in the game
+class EightGameNode:
+
+    def __init__(self, board, cost=1):
+        self.default_board = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+        self.empty_x = 2
+        self.empty_y = 2
+        self.cost = cost
+
+        if len(board) == 3 and len(board[0]) == 3 and len(board[1]) == 3 and len(board[2]) == 3:
+            self.board = board
+        else:
+            print("Invalid board")
+            self.board = self.default_board
+        self.initialize_empties()
+
+    # Finds the coordinates of the empty tile and saves them
+    def initialize_empties(self):
+        for row in range(len(self.board)):
+            for col in range(len(self.board[0])):
+                if self.board[row][col] == 0:
+                    self.empty_x = col
+                    self.empty_y = row
+
+    def copy(self):
+        new_board = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+        #new_board = copy.deepcopy(self.board)
+        for row in range(3):
+            for col in range(3):
+                new_board[row][col] = self.board[row][col]
+        new_node = EightGameNode(new_board, self.cost)
+        return new_node
+
+    def move_left(self):
+        if self.empty_x == 0:
+            return None
+        else:
+            result = self.copy()
+            result.board[self.empty_y][self.empty_x] = result.board[self.empty_y][self.empty_x-1]
+            result.board[self.empty_y][self.empty_x - 1] = 0
+            result.empty_x -= 1
+            result.set_cost(1)
+            return result
+
+    def move_right(self):
+        if self.empty_x == 2:
+            return None
+        else:
+            result = self.copy()
+            result.board[self.empty_y][self.empty_x] = result.board[self.empty_y][self.empty_x + 1]
+            result.board[self.empty_y][self.empty_x + 1] = 0
+            result.empty_x += 1
+            result.set_cost(1)
+            return result
+
+    def move_up(self):
+        if self.empty_y == 0:
+            return None
+        else:
+            result = self.copy()
+            result.board[self.empty_y][self.empty_x] = result.board[self.empty_y - 1][self.empty_x]
+            result.board[self.empty_y - 1][self.empty_x] = 0
+            result.empty_y -= 1
+            result.set_cost(1)
+            return result
+
+    def move_down(self):
+        if self.empty_y == 2:
+            return None
+        else:
+            result = self.copy()
+            result.board[self.empty_y][self.empty_x] = result.board[self.empty_y + 1][self.empty_x]
+            result.board[self.empty_y + 1][self.empty_x] = 0
+            result.empty_y += 1
+            result.set_cost(1)
+            return result
+
+    def __eq__(self, other):
+        if isinstance(other, EightGameNode):
+            for row in range(len(self.board)):
+                for col in range(len(self.board[0])):
+                    if self.board[col][row] != other.board[col][row]:
+                        return False
+            return True
+        return False
+
+    def equals(self, other_board):
+        for row in range(len(self.board)):
+            for col in range(len(self.board[0])):
+                if self.board[col][row] != other_board[col][row]:
+                    return False
+        return True
+
+    def set_cost(self, new_cost):
+        self.cost = new_cost
+
+    def __str__(self):
+        string = ""
+        for row in self.board:
+            string += str(row) + "\n"
+        string += "----------\n"
+        return string
+
+    def __hash__(self):
+        sum = 0
+        for row in range(len(self.board)):
+            for col in range(len(self.board[0])):
+                sum += col * self.board[row][col]
+        return sum
+
+    def hamming_distance(self, goal):
+        # TODO
+        return 0
+
+    def manhattan_distance(self, goal):
+        # TODO
+        return 0
+
+    def own_heuristic(self, goal):
+        # TODO
+        return 0
+
+
+
+
---
+++ Lab2/__init__.py
@@ -0,0 +1,32 @@
+from BestFirstSolver import BestFirstSolver
+from BreadthFirstSolver import BreadthFirstSolver
+from DepthFirstSolver import DepthFirstSolver
+from AStarSolver import AStarSolver
+from EightGameNode import EightGameNode
+from EightGameSpace import Eight_game_space
+
+## Start the Eight game problem sover with the selected board and problem solver
+
+
+board = [[1, 2, 3], [4, 5, 6], [7, 0, 8]]
+#board = [[1, 2, 3], [4, 0, 6], [7, 5, 8]]
+#board = [[1, 2, 3], [4, 0, 5], [6, 7, 8]]
+#board = [[1, 2, 0], [3, 4, 5], [6, 7, 8]]
+#board = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+try:
+    node = EightGameNode(board)
+    node.set_cost(0)
+    space = Eight_game_space()
+    solver = BreadthFirstSolver(space, node)
+    #solver = DepthFirstSolver(space, node)
+    #solver = BestFirstSolver(space, node)
+    # TODO
+    solution = solver.search()
+    if not(solution is None):
+        print(str(solution))
+        print("Cost: " + str(solution.cost))
+        print("Visited nodes: " + str(space.get_visited_count()))
+    else:
+        print("No solution")
+except BaseException as err:
+    print(err)
